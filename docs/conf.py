4e7f98545a1b9e1ec7b77bb353d20d9bb0726118
Lodewic <lvantwillert@qualogy.com>
2020-06-26 09:39:34 +0200

    docs updated css

--- docs/conf.py
+++ docs/conf.py
@@ -123,7 +123,9 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_css_files = [
+    'css/custom.css',
+]
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 # html_last_updated_fmt = '%b %d, %Y'
